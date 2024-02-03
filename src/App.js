50ba0fa358ee9fc7869caa4847852ac47adb5f95
quynhtran158 <quynhtran.3002@gmail.com>
2024-02-03 13:50:38 -0500

    second commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./Components/Home/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <h2> hihi </h2>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
